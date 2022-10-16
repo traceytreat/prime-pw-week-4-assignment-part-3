51aa90e74d7a93606205591724d659d9ef74d18d
Tracey Treat <tracey_treat@yahoo.com>
2022-10-16 14:33:45 -0500

    Added basket functions

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,30 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+
+function addItem ( item ){
+    basket.push(item);
+    return true;
+}
+
+function listItems ( ){
+    for (i of basket){
+        console.log(i);
+    }
+}
+
+function empty ( ){
+    basket = [];
+}
+
+//Testing
+console.log('Adding Banana to basket:', addItem('Banana'));
+console.log('Adding Apple to basket:', addItem('Apple'));
+console.log('Adding Bread to basket:', addItem('Bread'));
+
+console.log('Listing items in basket:');
+listItems();
+
+empty();
+console.log('Emptied basket:', basket);
